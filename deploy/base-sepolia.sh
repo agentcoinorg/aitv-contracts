28ab0d952808a98634120e611300989499a91e3d
nerfZael <bogunovij@gmail.com>
2024-09-04 19:12:49 +0200

    deployment to base sepolia

---
+++ deploy/base-sepolia.sh
@@ -0,0 +1,1 @@
+forge script ./script/DeployAgentKey.s.sol --rpc-url https://sepolia.base.org/ --broadcast --interactives 1 -g 200 --force --verify --verifier-url https://api-sepolia.basescan.org/api --etherscan-api-key $1
